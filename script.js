bc448833b8f68352ba272bb25354420f6aaa7d86
MxMacambira <ruanmacambira@gmail.com>
2021-05-26 14:33:08 -0300

    Component-like DOM elements creation

--- script.js
+++ script.js
@@ -17,83 +17,125 @@ function onDrop(event) {
     dropzone.appendChild(draggableElement);
 }
 
+function buildElement(
+    tag,
+    {
+        classList = [], attributes = {},
+        events = [], children = [],
+        ...rest
+    } = {}
+) {
+    let element = document.createElement(tag);
+
+    for(klass of classList) {
+        element.classList.add(klass);
+    }
+
+    for(let attr in attributes) {
+        const value = attributes[attr];
+        element.setAttribute(attr, value);
+    }
+
+    for(let ev in events) {
+        const callback = events[ev];
+        element[ev] = callback;
+    }
+
+    for(child of children) {
+        element.appendChild(child);
+    }
+
+    for(prop in rest) {
+        element[prop] = rest[prop];
+    }
+
+    return element;
+}
+
 function boardTemplate() {
-    let template = document.createElement('div');
-    template.classList.add('board');
-    template.ondragover = onDragOver;
-    template.ondrop = onDrop;
-
-    template.appendChild(boardHeaderTemplate());
-    template.appendChild(cardContainerTemplate());
-    template.appendChild(addCardTemplate());
-
-    return template;
+    return buildElement(
+        'div', {
+            classList: ['board'],
+            events: {
+                ondragover: onDragOver,
+                ondrop: onDrop
+            }, children: [
+                boardHeaderTemplate(),
+                cardContainerTemplate(),
+                addCardTemplate()
+            ]
+        }
+    )
 }
-
-function boardFormTemplate() {
-    let textForm = document.createElement('input');
-    textForm.placeholder = "Insira Título de Lista";
-    textForm.setAttribute('remote', true);
-
-    let applyButton = document.createElement('button');
-    applyButton.setAttribute('href', '#');
-    applyButton.innerHTML = "Criar"
-    applyButton.classList.add("apply-button");
-
-    let cancelButton = document.createElement('button');
-    cancelButton.setAttribute('href', '#');
-    cancelButton.innerHTML = "Cancelar";
-    cancelButton.classList.add("cancel-button");
-
-    let template = document.createElement('form');
-    template.classList.add('board-form');
-    template.appendChild(textForm);
-    template.appendChild(applyButton);
-    template.appendChild(cancelButton);
-
-    return template;
-}
-
 
 function boardHeaderTemplate() {
-    let template = document.createElement('h3');
-    template.classList.add('board-header');
-    template.innerHTML = "[Board Text]";
-
-    return template;
+    return buildElement(
+        'h3', {
+            classList: ['board-header'],
+            innerHTML: "[Board Text]",
+        }
+    )
 }
 
 function cardContainerTemplate() {
-    let template = document.createElement('div');
-    template.classList.add('card-container');
-    
-    return template;
+    return buildElement(
+        'div', {classList:['card-container']}
+    )
 }
 
 function cardTemplate() {
     if(!this.counter) { this.counter = 0 }
     this.counter += 1;
 
-    let template = document.createElement('div');
-    template.id = `drag-card-${this.counter}`;
-    template.classList.add('card');
-    template.setAttribute('draggable', true);
-    template.ondragstart = onDragStart;
-    template.innerHTML = "[Card Text]";
-
-    return template;
+    return buildElement(
+        'div', {
+            id: `drag-card=${this.counter}`,
+            classList: ['card'],
+            attributes: {draggable: true},
+            events: {ondragstart: onDragStart},
+            innerHTML: `[Card ${this.counter} Text]`
+        }
+    )
 }
 
 function addCardTemplate() {
-    let template = document.createElement('div');
-    template.innerHTML = "+ Adicionar outro item";
-    template.classList.add('new-card');
-    template.onclick = event => {
-        const board = event.target.parentNode;
-        board.appendChild(cardTemplate());
-        board.appendChild(event.target);
-    }
-    return template;
+    return buildElement(
+        'div', {
+            classList: ['new-card'],
+            innerHTML: "+ Adicionar outro item",
+            events: {
+                onclick: event => {
+                    const board = event.target.parentNode;
+                    board.appendChild(cardTemplate());
+                    board.appendChild(event.target);
+                }
+            }
+        }
+    )
+}
+
+function boardFormTemplate() {
+    const textForm = buildElement(
+        'input', {
+            placeholder: 'Insira Título da Lista',
+    })
+    const applyButton = buildElement(
+        'button', {
+            classList: ['apply-button'],
+            attributes: {href: '#'},
+            innerHTML: 'Criar'
+    })
+    const cancelButton = buildElement(
+        'button', {
+            classList: ['cancel-button'],
+            attributes: {href: '#'},
+            innerHTML: 'Cancelar',
+        })
+    return buildElement(
+        'form', {
+            classList: ['board-form'],
+            children: [textForm, applyButton, cancelButton]
+    })
 }
 
 window.onload = () => {
@@ -105,5 +147,5 @@ window.onload = () => {
         boardContainer.appendChild(event.target);
     }
 
-    // boardContainer.appendChild(boardFormTemplate());
+    boardContainer.appendChild(boardFormTemplate());
 }
